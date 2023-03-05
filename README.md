d4ece0b4002d390140ae753de0277eeb76726eb6
Daniel Augusto Mandira <daniel.mandira@hotmail.com>
2023-03-05 16:37:46 -0300

    teste

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Teste
-Testando 
+Aluno de Desenvolvimento de Sistemas 
---
+++ main.js
@@ -0,0 +1,15 @@
+function Dark(){
+            var corpo = document.getElementById('body');
+                corpo.style.cssText = 
+                'color: #FFF;'+
+                'background-color: rgb(29,29,39);';
+                text.style.cssText =
+                'color: #FFF;';
+            }
+            function Light(){
+
+                var corpo = document.getElementById('body');
+                corpo.style.cssText = 
+                'color: black;' +
+                'background-color: #FFF;';
+            }
